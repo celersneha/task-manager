fbf3d3d99772bd3150da842446d91db081f38ff5
Sneha Sharma <snehav2109@gmail.com>
2025-09-22 14:54:03 +0530

    Fix variable naming for authentication state in Home component

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -2,7 +2,7 @@ import HeroSection from "@/components/ui/Home/HeroSection";
 import { useState } from "react";
 
 function Home() {
-  const [isAuthOpen, setIsAuthOpen] = useState(false);
+  const [_isAuthOpen, setIsAuthOpen] = useState(false);
   return (
     <>
       <HeroSection onAuthOpen={() => setIsAuthOpen(true)} />
